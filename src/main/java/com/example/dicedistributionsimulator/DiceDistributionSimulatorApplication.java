a53bf0e4b6093f7f6371783c6a8d7e149453846c
Sebastian Pilarczyk <sebastian.pilarczyk@gmail.com>
2021-09-04 11:54:15 +0200

    Init project

---
+++ src/main/java/com/example/dicedistributionsimulator/DiceDistributionSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.dicedistributionsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceDistributionSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiceDistributionSimulatorApplication.class, args);
+    }
+
+}
